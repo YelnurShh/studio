b1f1616e578a40121ad949fbd34926fed6fe8af0
YelnurShh <yelnurshakhar@gmail.com>
2026-04-10 10:11:13 +0500

    studio

--- next.config.js
+++ next.config.js
@@ -8,6 +8,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/og-image.png',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
